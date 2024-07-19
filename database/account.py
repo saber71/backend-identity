cd615a4e12413f7854094bf047a96edfee94ecf5
saber71 <1533357473@qq.com>
2024-07-19 15:22:26 +0800

    feat(backend-identity): generate UUID for account IDs and refactor storage communication
    - Generate UUID for account IDs instead of using auto-incrementing integers.
    - Refactor the way accounts are stored and deleted, replacing direct storage calls with requests to an auth service.
    - Improve error handling during account creation by checking for 'id' in properties.
    - Update Pipfile.lock to reflect new backend-storage version and added test command.
    
    BREAKING CHANGE: Account IDs are now UUIDs instead of auto-incrementing integers.
    This may affect any code that depends on account IDs being integers.

--- database/account.py
+++ database/account.py
@@ -8,7 +8,7 @@ import database
 class Account(database.Base):
     __tablename__ = "account"
 
-    id = mapped_column(String, primary_key=True, autoincrement=True)
+    id = mapped_column(String, primary_key=True)
     name = mapped_column(String, nullable=False, unique=True, comment="账号名")
     create_time = mapped_column(DateTime, comment="创建时间")
     update_time = mapped_column(DateTime, comment="更新时间")
--- routes/account.py
+++ routes/account.py
@@ -1,7 +1,9 @@
 import datetime
+import uuid
 from typing import Optional
 
 import fastapi
+import requests
 import storage
 from fastapi import APIRouter
 from pydantic import BaseModel
@@ -23,33 +25,29 @@ class CreateAccount(BaseModel):
 # 定义创建账户的API路由
 @router.post("/create")
 def create(data: CreateAccount):
-    if data.properties and "id" in data.properties:
-        raise fastapi.HTTPException(status_code=422, detail="properties中不能包含有id")
     """
     创建一个新的账户。
 
     使用提供的账户名称和密码创建一个账户实体，并将其存储在数据库中。
-    同时，将账户ID和密码存储在持久化存储中。
+    同时，将其他属性存储在持久化存储中。
 
     参数:
     - data: CreateAccount类型的实例，包含账户名称和密码。
     """
-    with database.begin() as session:
+    if data.properties and "id" in data.properties:
+        raise fastapi.HTTPException(status_code=422, detail="properties中不能包含有id")
+    with database.begin() as session, storage.TransactionContext() as ctx:
         # 创建一个新的账户实体
         account = Account(
+            id=str(uuid.uuid4()),
             name=data.name,
             update_time=datetime.datetime.now(),
             create_time=datetime.datetime.now(),
         )
         # 将账户实体添加到数据库会话中，准备插入数据库
         session.add(account)
-        storage.save(
-            {
-                "name": constants.STORAGE_NAME,
-                "value": [{"id": account.id, "password": data.password}],
-            }
-        )
-        storage.save(
+        requests.post("/auth/save", data={"id": account.id, "password": data.password})
+        ctx.save(
             {
                 "name": constants.STORAGE_ACCOUNT_PROPERTIES_NAME,
                 "value": [
@@ -61,17 +59,17 @@ def create(data: CreateAccount):
 
 # 定义删除账户的API路由
 @router.post("/delete")
-def remove(id: str):
+def delete(id: str):
     """
     删除一个账户。
 
     根据提供的账户ID从数据库中删除对应的账户实体。
-    同时，从持久化存储中删除账户的ID和密码。
+    同时，从持久化存储中删除账户的ID和密码和相关属性。
 
     参数:
     - id: 要删除的账户的ID。
     """
-    with database.begin() as session:
+    with database.begin() as session, storage.TransactionContext() as ctx:
         session.query(Account).filter_by(id=id).delete()
-        storage.delete({"name": constants.STORAGE_NAME, "id": id})
-        storage.delete({"name": constants.STORAGE_ACCOUNT_PROPERTIES_NAME, "id": id})
+        requests.post("/auth/delete", params={"id": id})
+        ctx.delete({"name": constants.STORAGE_ACCOUNT_PROPERTIES_NAME, "id": id})
